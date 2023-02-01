141efc2d8fd7466073cd984e284b5c51786304a7
yandong1024 <123480002+yandong1024@users.noreply.github.com>
2023-02-01 12:05:23 +0800

    Update 01_customize_packages.sh

--- scripts/01_customize_packages.sh
+++ scripts/01_customize_packages.sh
@@ -6,7 +6,7 @@
 # PassWall 
 cp -rf ../passwall-packages/*  package/new/
 cp -rf ../passwall-luci/*  package/new/
-rm -rf ./feeds/packages/net/xray-core
+rm -rf package/new/xray-core
 
 # Access Control
 cp -rf ../immortalwrt-luci/applications/luci-app-accesscontrol package/new/
