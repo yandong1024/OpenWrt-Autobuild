138cd95166a8a5ded59038bb6ddb70dab64677fd
clown <458892+aieu@users.noreply.github.com>
2024-07-23 12:58:58 +0800

    refine build env

--- scripts/00_init.sh
+++ scripts/00_init.sh
@@ -3,10 +3,6 @@
 set -ex
 
 
-__get_openwrt() {
-    git clone https://github.com/openwrt/openwrt
-}
-
 __get_other-repos() {
     git clone -b master --depth 1 --single-branch https://github.com/coolsnowwolf/lede lede
     git clone -b master --depth 1 --single-branch https://github.com/immortalwrt/immortalwrt immortalwrt
@@ -14,32 +10,89 @@ __get_other-repos() {
     git clone -b master --depth 1 --single-branch https://github.com/immortalwrt/luci immortalwrt-luci
 }
 
-__init_build_env() {
-    sudo swapoff -a
-    sudo rm -rf /etc/apt/sources.list.d /usr/share/dotnet /usr/local/lib/android /opt/ghc
-    sudo -E apt-get -qq update
-    sudo -E apt-get -qq install aria2
-    sudo -E wget -P /usr/local/sbin/ https://github.com/HiGarfield/lede-17.01.4-Mod/raw/master/.github/backup/apt-fast
-    sudo -E chmod -R 755 /usr/local/sbin/apt-fast
-    sudo -E apt-fast -y -qq install ack antlr3 aria2 asciidoc autoconf automake autopoint binutils bison btrfs-progs build-essential bzip2 ca-certificates ccache cmake coreutils cpio curl device-tree-compiler fastjar flex g++-multilib gawk gcc-multilib gettext git git-core gperf gzip haveged help2man intltool img2simg jq libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev libreadline-dev libssl-dev libtool libz-dev lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch pigz pkgconf python2.7 python3 python3-pip python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools swig tar texinfo uglifyjs unzip upx upx-ucl vim wget xmlto xsltproc xxd xz-utils yasm zip zlib1g-dev zstd
-    sudo -E pip3 install pyelftools pylibfdt
-    sudo -E apt-get -qq autoremove --purge
-    sudo -E apt-get -qq clean
-    sudo -E git config --global user.name 'GitHub Actions' && git config --global user.email 'noreply@github.com'
-    df -h
+__patch_fullconenat() {
+    # patch kernel
+    cp -f ./lede/target/linux/generic/hack-6.6/952-add-net-conntrack-events-support-multiple-registrant.patch ./openwrt/target/linux/generic/hack-6.6/
+    # disable KERNEL_WERROR
+    sed -i 's,imply KERNEL_WERROR,#imply KERNEL_WERROR,g' ./openwrt/toolchain/gcc/Config.version
+    # fullconenat-nft
+    cp -rf ./immortalwrt/package/network/utils/fullconenat-nft ./openwrt/package/network/utils/
+    # libnftnl
+    rm -rf ./openwrt/package/libs/libnftnl
+    cp -rf ./immortalwrt/package/libs/libnftnl ./openwrt/package/libs/
+    # nftables
+    rm -rf ./openwrt/package/network/utils/nftables/
+    cp -rf ./immortalwrt/package/network/utils/nftables ./openwrt/package/network/utils/
+    # firewall4
+    rm -rf ./openwrt/package/network/config/firewall4
+    cp -rf ./immortalwrt/package/network/config/firewall4 ./openwrt/package/network/config/
+    # patch luci
+    patch -d ./openwrt/feeds/luci -p1 -i ../../../patches/fullconenat-luci.patch
 }
 
-__init_feeds() {
-    ./openwrt/scripts/feeds update -a && ./openwrt/scripts/feeds install -a
+__burn_onecloud() {
+    ver="v0.3.1"
+    curl -L -o ./AmlImg https://github.com/hzyitc/AmlImg/releases/download/$ver/AmlImg_${ver}_linux_amd64
+    chmod +x ./AmlImg
+
+    curl -L -o ./uboot.img https://github.com/hzyitc/u-boot-onecloud/releases/download/build-20221028-0940/eMMC.burn.img
+    ./AmlImg unpack ./uboot.img burn/
+    echo "::endgroup::"
+    gunzip -k openwrt/bin/targets/*/*/*.gz
+    diskimg=$(ls openwrt/bin/targets/*/*/*.img)
+    loop=$(sudo losetup --find --show --partscan $diskimg)
+    img_ext="openwrt.img"
+    img_mnt="xd"
+    rootfs_mnt="img"
+    boot_img=$1${img_ext}
+    boot_img_mnt=$1${img_mnt}
+    rootfs_img_mnt=$1${rootfs_mnt}
+    echo ${boot_img}
+    echo ${boot_img_mnt}
+    echo ${rootfs_img_mnt}
+    sudo rm -rf ${boot_img}
+    sudo rm -rf ${boot_img_mnt}
+    sudo rm -rf ${rootfs_img_mnt}
+    sudo dd if=/dev/zero of=${boot_img} bs=1M count=400
+    sudo mkfs.ext4 ${boot_img}
+    sudo mkdir ${boot_img_mnt}
+    sudo mkdir ${rootfs_img_mnt}
+    sudo mount ${boot_img} ${boot_img_mnt}
+    sudo mount ${loop}p2 ${rootfs_img_mnt}
+
+    pushd ${rootfs_img_mnt}
+    sudo cp -r * ../${boot_img_mnt}
+    sudo sync
+    popd
+
+    sudo umount ${boot_img_mnt}
+    sudo umount ${rootfs_img_mnt}
+
+    sudo img2simg ${loop}p1 burn/boot.simg
+    sudo img2simg openwrt.img burn/rootfs.simg
+
+    sudo rm -rf *.img
+    sudo losetup -d $loop
+    cat <<EOF >>burn/commands.txt
+PARTITION:boot:sparse:boot.simg
+PARTITION:rootfs:sparse:rootfs.simg
+EOF
+    prefix=$(ls openwrt/bin/targets/*/*/*.img | sed 's/sdcard.img$/emmc/')
+    burnimg=${prefix}.img
+    ./AmlImg pack $burnimg burn/
+
+    for f in openwrt/bin/targets/*/*/*-emmc.img; do
+        gzip "$f"
+    done
+    sudo rm -rf openwrt/bin/targets/*/*/*.img
 }
 
 
 case $1 in
-    build_env)      __init_build_env    ;;
-    openwrt)        __get_openwrt       ;;
-    other-repos)    __get_other-repos   ;;
-    feeds)          __init_feeds        ;;
-    *)              echo "input error"  ;;
+    other-repos)        __get_other-repos   ;;
+    patch-fullconenat)  __patch_fullconenat ;;
+    burn-onecloud)      __burn_onecloud     ;;
+    *)                  echo "input error"  ;;
 esac
 
 exit 0
